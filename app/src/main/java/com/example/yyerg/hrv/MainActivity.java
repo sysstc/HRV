458fe983e265f2d5b58d64b20eccd5018a3bd12e
HuangChung-Hao重豪 <HuangChung-Hao>
2016-04-24 23:09:48 +0800

    animation

--- app/src/main/java/com/example/yyerg/hrv/MainActivity.java
+++ app/src/main/java/com/example/yyerg/hrv/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final String APP_TAG = "HRV";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/yyerg/hrv/MainActivityFragment.java
+++ app/src/main/java/com/example/yyerg/hrv/MainActivityFragment.java
@@ -4,8 +4,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
 import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +16,13 @@ import android.widget.LinearLayout.LayoutParams;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.chart.BarChart.Type;
+import org.achartengine.model.CategorySeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -42,85 +45,108 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        int i;
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         RR = new ArrayList<Integer>();
-        RR.add(10);
-        RR.add(11);
-        RR.add(12);
-                llBarChart = (LinearLayout) rootView.findViewById(R.id.llBarChart);
+        for(i=0;i<30;i++){
+            RR.add(0);
+        }
+        llBarChart = (LinearLayout) rootView.findViewById(R.id.llBarChart);
         rand = new Random();
         try{
-            vChart = getBarChart("RR Interval", "ErrCode", "QTY", RR);
+            vChart = getBarChart(RR);
             llBarChart.removeAllViews();
-            //llBarChart.addView(vChart);
             llBarChart.addView(vChart, new LayoutParams(LayoutParams.WRAP_CONTENT, 300));
         }catch(Exception e){
 
         }
         ScheduledExecutorService scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
         final ScheduledFuture drawHandle =
-                scheduleTaskExecutor.scheduleAtFixedRate(drawThread, 5, 5, TimeUnit.SECONDS);
+                scheduleTaskExecutor.scheduleAtFixedRate(drawThread, 1, 1, TimeUnit.SECONDS);
+
         return rootView;
+
     }
-
-
 
     private Runnable drawThread = new Runnable() {
         public void run() {
             int i = rand.nextInt(1000 - 700) + 700;
+            Log.d(MainActivity.APP_TAG, "add: " + Integer.toString(i));
+            if(RR.size()==30) {
+                RR.remove(0);
+            }
             RR.add(i);
-            getBarChart("RR Interval", "ErrCode", "QTY", RR);
+            try{
+                Log.d(MainActivity.APP_TAG, "getBarChart");
+            }catch(Exception e){
+
+            }
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    vChart = getBarChart(RR);
+                    llBarChart.removeAllViews();
+                    llBarChart.addView(vChart, new LayoutParams(LayoutParams.WRAP_CONTENT, 300));
+                }
+            });
         }
     };
 
-    private View getBarChart(String chartTitle, String XTitle, String YTitle, ArrayList<Integer> rr){
-
-        XYSeries Series = new XYSeries(YTitle);
-
-        XYMultipleSeriesDataset Dataset = new XYMultipleSeriesDataset();
-        Dataset.addSeries(Series);
-
-        XYMultipleSeriesRenderer Renderer = new XYMultipleSeriesRenderer();
-        XYSeriesRenderer yRenderer = new XYSeriesRenderer();
-        Renderer.addSeriesRenderer(yRenderer);
-
-        //Renderer.setApplyBackgroundColor(true);			//設定背景顏色
-        //Renderer.setBackgroundColor(Color.BLACK);			//設定圖內圍背景顏色
-        Renderer.setMarginsColor(Color.WHITE);				//設定圖外圍背景顏色
-        Renderer.setTextTypeface(null, Typeface.BOLD);		//設定文字style
-
-        Renderer.setShowGrid(true);							//設定網格
-        Renderer.setGridColor(Color.GRAY);					//設定網格顏色
-
-        Renderer.setChartTitle(chartTitle);					//設定標頭文字
-        Renderer.setLabelsColor(Color.BLACK);				//設定標頭文字顏色
-        Renderer.setChartTitleTextSize(20);					//設定標頭文字大小
-        Renderer.setAxesColor(Color.BLACK);				//設定雙軸顏色
-        Renderer.setBarSpacing(0.5);						//設定bar間的距離
-
-        //Renderer.setXTitle(XTitle);						//設定X軸文字
-        //Renderer.setYTitle(YTitle);						//設定Y軸文字
-        Renderer.setXLabelsColor(Color.BLACK);				//設定X軸文字顏色
-        Renderer.setYLabelsColor(0, Color.BLACK);			//設定Y軸文字顏色
-        Renderer.setXLabelsAlign(Align.CENTER);			//設定X軸文字置中
-        Renderer.setYLabelsAlign(Paint.Align.CENTER);		//設定Y軸文字置中
-        Renderer.setXLabelsAngle(-25); 						//設定X軸文字傾斜度
-
-        Renderer.setXLabels(0); 							//設定X軸不顯示數字, 改以程式設定文字
-        Renderer.setYAxisMin(0);							//設定Y軸文最小值
-
-        yRenderer.setColor(Color.RED);              		//設定Series顏色
-        //yRenderer.setDisplayChartValues(true);			//展現Series數值
-
-        Series.add(0, 0);
-        Renderer.addXTextLabel(0, "");
-        for(int r=0; r<rr.size(); r++) {
-            //Log.i("DEBUG", (r+1)+" "+xy[r][0]+"; "+xy[r][1]);
-            Renderer.addXTextLabel(r+1, rr.get(r).toString());
-            Series.add(r+1, rr.get(r));
+    protected XYMultipleSeriesRenderer buildBarRenderer(int[] colors) {
+        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+        renderer.setAxisTitleTextSize(16);
+        renderer.setChartTitleTextSize(20);
+        renderer.setLabelsTextSize(15);
+        renderer.setLegendTextSize(15);
+        int length = colors.length;
+        for (int i = 0; i < length; i++) {
+            XYSeriesRenderer r = new XYSeriesRenderer();
+            r.setColor(colors[i]);
+            renderer.addSeriesRenderer(r);
         }
+        return renderer;
+    }
 
-        View view = ChartFactory.getBarChartView(getActivity(), Dataset, Renderer, Type.DEFAULT);
-        return view;
+    protected void setChartSettings(XYMultipleSeriesRenderer renderer, String title, String xTitle,
+                                    String yTitle, double xMin, double xMax, double yMin, double yMax, int axesColor,
+                                    int labelsColor) {
+        renderer.setChartTitle(title);
+        renderer.setXTitle(xTitle);
+        renderer.setYTitle(yTitle);
+        renderer.setXAxisMin(xMin);
+        renderer.setXAxisMax(xMax);
+        renderer.setYAxisMin(yMin);
+        renderer.setYAxisMax(yMax);
+        renderer.setAxesColor(axesColor);
+        renderer.setLabelsColor(labelsColor);
+    }
+
+    public XYMultipleSeriesDataset buildBarDataset(String title, ArrayList<Integer> values) {
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+        CategorySeries series = new CategorySeries(title);
+        int seriesLength = values.size();
+        Log.d(MainActivity.APP_TAG, "length: " + Integer.toString(values.size()));
+        for (int k = 0; k < seriesLength; k++) {
+            series.add(values.get(k));
+        }
+        dataset.addSeries(series.toXYSeries());
+        return dataset;
+    }
+
+    private View getBarChart(ArrayList<Integer> RR){
+        String title = "RR";
+        int[] barColors = new int[] { Color.CYAN};
+
+        XYMultipleSeriesRenderer renderer = buildBarRenderer(barColors);
+        renderer.setOrientation(XYMultipleSeriesRenderer.Orientation.HORIZONTAL);
+
+        setChartSettings(renderer, "RR", "time", "RR interval", 0.5,
+                30.5, 0, 1000, Color.GRAY, Color.RED);
+        renderer.setXLabels(1);
+        renderer.setYLabels(10);
+        XYSeriesRenderer seriesRenderer = (XYSeriesRenderer) renderer.getSeriesRendererAt(0);
+        seriesRenderer.setDisplayChartValues(true);
+        return ChartFactory.getBarChartView(this.getActivity(), buildBarDataset(title, RR), renderer,
+                Type.DEFAULT);
     }
 }
